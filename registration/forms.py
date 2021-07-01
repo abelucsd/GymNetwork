fa71b0c53cc146873cf64ab9225acb74b8aaea56
Angelo <belmonte.amp@gmail.com>
2021-07-01 14:11:51 -0400

    Create Account & Login Finish

--- registration/forms.py
+++ registration/forms.py
@@ -3,11 +3,15 @@ from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
-class RegistrationForm(UserCreationForm):
+class RegistrationForm(UserCreationForm):	
 	email = forms.EmailField()
 
 	class Meta:
 		model = User
 		fields = ["username", "email", "password1", "password2"]
 
-	
+	def __init__(self, *args, **kwargs):
+		super(RegistrationForm, self).__init__(*args, **kwargs)
+
+		self.fields["password1"].help_text = None
+
